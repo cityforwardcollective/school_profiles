d215977c6b56c196822e30c1233e786412d74476
Spencer Schien <spencer.schien@gmail.com>
2024-02-01 08:33:50 -0600

    compiled reports

--- R/make_reports.R
+++ R/make_reports.R
@@ -1,29 +1,87 @@
 library(tidyverse)
 library(quarto)
 library(glue)
+library(parallel)
+library(doParallel)
+library(furrr)
+library(progressr)
+library(tictoc)
 
 leges_sf <- read_rds("data/electeds_with_sf_2023.rda")
 
-rep <- "Jonathan Brostoff"
-rind <- which(leges_sf$name == rep)
-district <- leges_sf[[rind,"district"]] 
-hon <- leges_sf[[rind,"title"]] 
-house <- leges_sf[[rind,"house"]] 
 
-
-of <- glue("{rep} - District {district}.pdf")
-quarto::quarto_render("template_report/template_report.qmd", 
-                      execute_params = list("representative" = rep,
-                                            "district" = district,
-                                            "honorific" = hon,
-                                            "house" = house), 
-                      output_file = of)
-
-d <- glue("compiled_reports/{house}")
-
-if (!dir.exists(d)) {
-  dir.create(d)
+make_reports <- function(i) {
+  this <- leges_sf[i,]
+  rep <- this[["name"]]
+  district <- this[["district"]] 
+  hon <- this[["title"]] 
+  house <- this[["house"]] 
+  
+  of <- glue("{rep} - District {district}.pdf")
+  quarto_render("template_report/template_report.qmd", 
+                execute_params = list("representative" = rep,
+                                      "district" = district,
+                                      "honorific" = hon,
+                                      "house" = house), 
+                output_file = of)
+  
+  d <- glue("compiled_reports/{house}")
+  
+  if (!dir.exists(d)) {
+    dir.create(d)
+  }
+  
+  file.copy(from = of, to = glue("{d}/{of}"), overwrite = TRUE)
+  file.remove(of)
 }
 
-file.copy(from = of, to = glue("{d}/{of}"), overwrite = TRUE)
-file.remove(of)
+# make_reports(9)
+
+
+plan(multicore)
+
+this_fun <- function(i) {
+  p <- progressor(steps = length(i))
+  
+  future_walk(i, .options = furrr_options(scheduling = 2), function(i) {
+    p()
+    this <- leges_sf[i,]
+    rep <- this[["name"]]
+    district <- this[["district"]] 
+    hon <- this[["title"]] 
+    house <- this[["house"]] 
+    
+    of <- glue("{rep} - District {district}.pdf")
+    quarto_render("template_report/template_report.qmd", 
+                  execute_params = list("representative" = rep,
+                                        "district" = district,
+                                        "honorific" = hon,
+                                        "house" = house), 
+                  output_file = of)
+    
+    d <- glue("compiled_reports/{house}")
+    
+    if (!dir.exists(d)) {
+      dir.create(d)
+    }
+    
+    file.copy(from = of, to = glue("{d}/{of}"), overwrite = TRUE)
+    file.remove(of)
+  })
+}
+
+
+
+
+tic()
+with_progress({
+  this_fun(1:nrow(leges_sf))
+})
+toc()
+
+tic()
+with_progress({
+  other_fun()
+})
+toc()
+
