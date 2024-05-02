7486db7d517b5177685a27f5f74c7f0f0c1cf108
Spencer Schien <spencer.schien@gmail.com>
2024-05-02 13:07:09 -0500

    updated with new maps

--- R/make_reports.R
+++ R/make_reports.R
@@ -11,41 +11,42 @@ library(cityforwardcollective)
 leges_sf <- read_rds("data/electeds_with_sf_2024.rda")
 
 
-make_reports <- function(i) {
-  this <- leges_sf[i,]
-  rep <- this[["name"]]
-  district <- this[["district"]] 
-  hon <- this[["title"]] 
-  house <- this[["house"]] 
-  
-  of <- glue("{rep} - District {district}.pdf")
-  d <- glue("compiled_reports/{house}")
-  
-  if (!dir.exists(d)) {
-    dir.create(d)
-  }
-  
-  if (!file.exists(glue("{d}/{of}"))) {
-    quarto_render("template_report/template_report.qmd", 
-                  execute_params = list("representative" = rep,
-                                        "district" = district,
-                                        "honorific" = hon,
-                                        "house" = house), 
-                  output_file = of)
-    
-    
-    file.copy(from = of, to = glue("{d}/{of}"), overwrite = TRUE)
-    file.remove(of)
-  }
-  
-}
+
+# make_reports <- function(i) {
+#   this <- leges_sf[i,]
+#   rep <- this[["name"]]
+#   district <- this[["district"]] 
+#   hon <- this[["title"]] 
+#   house <- this[["house"]] 
+#   
+#   of <- glue("{rep} - District {district}.pdf")
+#   d <- glue("compiled_reports/{house}")
+#   
+#   if (!dir.exists(d)) {
+#     dir.create(d)
+#   }
+#   
+#   if (!file.exists(glue("{d}/{of}"))) {
+#     quarto_render("template_report/template_report.qmd", 
+#                   execute_params = list("representative" = rep,
+#                                         "district" = district,
+#                                         "honorific" = hon,
+#                                         "house" = house), 
+#                   output_file = of)
+#     
+#     
+#     file.copy(from = of, to = glue("{d}/{of}"), overwrite = TRUE)
+#     file.remove(of)
+#   }
+#   
+# }
 
 # make_reports(9)
 
 
 # plan(multicore)
 
-this_fun <- function(i) {
+this_fun <- function(i, overwrite = FALSE) {
   p <- progressor(steps = length(i))
   
   future_walk(i, .options = furrr_options(scheduling = 2), function(i) {
@@ -63,7 +64,21 @@ this_fun <- function(i) {
       dir.create(d)
     }
     
-    if (!file.exists(glue("{d}/{of}"))) {
+    if (!overwrite) {
+      if (!file.exists(glue("{d}/{of}"))) {
+        quarto_render("template_report/template_report.qmd", 
+                      execute_params = list("representative" = rep,
+                                            "district" = district,
+                                            "honorific" = hon,
+                                            "house" = house), 
+                      output_file = of)
+        
+        
+        
+        file.copy(from = of, to = glue("{d}/{of}"), overwrite = TRUE)
+        file.remove(of)
+      }
+    } else {
       quarto_render("template_report/template_report.qmd", 
                     execute_params = list("representative" = rep,
                                           "district" = district,
@@ -76,24 +91,19 @@ this_fun <- function(i) {
       file.copy(from = of, to = glue("{d}/{of}"), overwrite = TRUE)
       file.remove(of)
     }
+    
       
   })
 }
 
-list.files("compiled_reports/Assembly") |> 
-  length()
 
 
+leges_sf <- leges_sf |> 
+  filter(house %in% c("Senate", "Assembly"))
 
-tic()
+
 with_progress({
-  this_fun(nrow(leges_sf):1)
+  this_fun(1:nrow(leges_sf), overwrite = TRUE)
 })
-toc()
 
-# tic()
-# with_progress({
-#   other_fun()
-# })
-# toc()
 
